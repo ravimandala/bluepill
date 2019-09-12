811e3232d33882d8dc494dad0ed180fe43e2a2a3
Brian Dittmer <bdittmer@linkedin.com>
2019-09-12 11:29:59 -0700

    Add additional keys

--- bp/src/BPXCTestFile.h
+++ bp/src/BPXCTestFile.h
@@ -38,10 +38,6 @@
                               andXcodePath:(NSString *)xcodePath
                                   andError:(NSError **)errPtr;
 
-+ (instancetype)BPXCTestFileFromBPTestPlan:(BPTestPlan *) testPlan
-                              andXcodePath:(NSString *)xcodePath
-                                  andError:(NSError **)errPtr;
-
 - (NSUInteger)numTests;
 - (NSArray *)allTestCases;
 - (void)listTestClasses;
